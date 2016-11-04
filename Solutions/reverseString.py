37e473f8c2d4b42bff9a719e23dbb5617e2ac416
Wahe3bru <wahe3bru@gmail.com>
2016-11-04 12:19:28 +0200

    created folder of my solutions
    to look back and mock

---
+++ Solutions/reverseString.py
@@ -0,0 +1,10 @@
+#Reverse a String - Enter a string and the program will reverse it
+#and print it out
+
+si = "sdrawkcab dootsrednu dna sdrawrof devil eb ylno nac efil"
+l = len(si)
+reversi =""
+while  l:
+    l-=1
+    reversi += si[l]
+print(reversi)
